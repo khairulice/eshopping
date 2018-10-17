8f65d4c6dc36ba30007258f077fd1b3790b86228
khairul <khairulice@gmail.com>
2018-10-17 23:35:46 +0600

    Init

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
     render() {
-        return (<h2>Home page</h2>)
+        return (<h2>Home page info</h2>)
     }
 }
