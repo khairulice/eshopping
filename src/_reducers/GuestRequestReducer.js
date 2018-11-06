5d33101594c1f14bdfe6cece1fc5b676b68a973c
Khairul <khairulice@gmail.com>
2018-11-06 17:09:49 +0600

    formatting

--- src/_reducers/GuestRequestReducer.js
+++ src/_reducers/GuestRequestReducer.js
@@ -1,10 +1,10 @@
 import { guestRequestConstants } from '../_constants';
 
-export function guestRequestReducer(state = { completed: 0,pending:0 }, action) {
+export function guestRequestReducer(state = { completed: 0, pending: 0 }, action) {
     switch (action.type) {
         case guestRequestConstants.GUEST_REQUEST_COMPLETED:
             return { ...state, completed: action.completed };
-            case guestRequestConstants.GUEST_REQUEST_PENDING:
+        case guestRequestConstants.GUEST_REQUEST_PENDING:
             return { ...state, pending: action.pending };
         default:
             return state
